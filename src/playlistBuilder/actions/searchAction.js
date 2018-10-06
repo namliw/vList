f08d19124134a947489f6f2b695584d9fd6ce4f3
namliw <willman.castaneda@gmail.com>
2018-10-06 19:06:54 +1000

    adde YT key to ignored config file

--- src/playlistBuilder/actions/searchAction.js
+++ src/playlistBuilder/actions/searchAction.js
@@ -2,14 +2,14 @@ import axios from "axios/index";
 export const YT_SEARCH_VIDEOS = 'YT_SEARCH_VIDEOS';
 export const YT_ADD_TO_PLAYLIST = 'YT_PLAYLIST_ADD';
 export const YT_REMOVE_FROM_RESULTS = 'YT_REMOVE';
-const API_KEY = '[API_KEY]';
+import {YT_API_KEY} from "../../configs";
 const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 export function searchVideos(searchParam, nextPageKey) {
 
     const params = {
         part: 'snippet',
-        key: API_KEY,
+        key: YT_API_KEY,
         q: searchParam,
         type: 'video'
     };
