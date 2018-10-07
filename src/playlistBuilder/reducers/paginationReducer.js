18538cd427a3b0a169aa4a2b29d65c80f86b6937
namliw <willman.castaneda@gmail.com>
2018-10-07 15:40:52 +1100

    fixed pagination

--- src/playlistBuilder/reducers/paginationReducer.js
+++ src/playlistBuilder/reducers/paginationReducer.js
@@ -1,12 +1,12 @@
 //reducers are always functions
-import {YT_SEARCH_VIDEOS} from '../actions/searchAction';
+import {YT_SEARCH_VIDEOS, YT_LOAD_MORE} from '../actions/searchAction';
 
-export default function (state = [], action) {
+export default function (state = '', action) {
     console.log(action.type, action.payload);
     switch (action.type) {
         case YT_SEARCH_VIDEOS:
+        case YT_LOAD_MORE:
             return action.payload.data.nextPageToken;
-            break;
     }
     return state;
 }
